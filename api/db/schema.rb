7fb0aa9f76d2d369c2887bcd59ce1ff08f75ed9d
learn <miss.nadia.lee@gmail.com>
2019-04-13 16:41:45 -0700

    Add img to Contact

--- api/db/schema.rb
+++ api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_13_165608) do
+ActiveRecord::Schema.define(version: 2019_04_13_225720) do
 
   create_table "contacts", force: :cascade do |t|
     t.string "first_name"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2019_04_13_165608) do
     t.string "work_address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "img"
   end
 
   create_table "wires", force: :cascade do |t|

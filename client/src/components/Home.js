9c0fb842fa3192cd56cd8ff6cb3096e0f21ac6d3
learn <miss.nadia.lee@gmail.com>
2019-04-15 20:57:06 -0700

    Create Home component

---
+++ client/src/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className="Home">
+            <h2>Welcome to wiretap!</h2>
+        </div>
+    );
+};
+
+export default Home;

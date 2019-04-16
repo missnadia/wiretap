6c554833fa10cd5e9a8ec231a460ba03c2101b53
learn <miss.nadia.lee@gmail.com>
2019-04-15 20:58:37 -0700

    Add delete button to ContactCard

--- client/src/components/ContactCard.js
+++ client/src/components/ContactCard.js
@@ -3,10 +3,16 @@ import React from 'react'
 const ContactCard = ({ contact }) => (
     <div className="ContactCard">
         <div className="ContactCardImg">
-            <img src={contact.img} alt={contact.name} />
+            <img
+                src={contact.img}
+                alt={contact.name}
+            />
         </div>
         <h3 className="ContactName">{contact.first_name} {contact.last_name}</h3>
+        <button className="DeleteButton">Delete Contact</button>
     </div>
+
+
 )
 
 export default ContactCard

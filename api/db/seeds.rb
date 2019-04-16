d6dc908a29d2f046e207a43d09016017db927f15
learn <miss.nadia.lee@gmail.com>
2019-04-15 18:34:38 -0700

    Add additional seed data to Rails db

--- api/db/seeds.rb
+++ api/db/seeds.rb
@@ -17,5 +17,33 @@ Contact.create(
         home_address: '123 Land of Oo',
         work_address: '123 Land of Oo',
         img: 'https://t6.rbxcdn.com/1fe03c714e0d3976e6c1207759e39b90'
+    },
+    {
+        first_name: "Jake",
+        last_name: "Dog",
+        email: "jakethedog@at.com",
+        mobile: 123456789,
+        work: 123456789,
+        home: 123456789,
+        fax: 123456789,
+        home_address: "Land of Oo",
+        work_address: "Land of Oo",
+        created_at: "2019-04-13T19:37:15.448Z",
+        updated_at: "2019-04-13T23:45:08.599Z",
+        img: "https://vignette.wikia.nocookie.net/parody/images/f/fb/JakeRJS.png/revision/latest?cb=20141209233937"
+    },
+    {
+        first_name: "Princess Bonnibel",
+        last_name: "Bubblegum",
+        email: "princess.bubblegum@at.com",
+        mobile: 123456789,
+        work: 123456789,
+        home: 123456789,
+        fax: 123456789,
+        home_address: "The Candy Castle",
+        work_address: "The Candy Castle",
+        created_at: "2019-04-13T23:19:30.019Z",
+        updated_at: "2019-04-13T23:28:03.584Z",
+        img: "https://banner2.kisspng.com/20180424/vgq/kisspng-princess-bubblegum-marceline-the-vampire-queen-fin-love-for-all-seasons-5adff241c52394.0726832015246259858075.jpg"
     }
 )
